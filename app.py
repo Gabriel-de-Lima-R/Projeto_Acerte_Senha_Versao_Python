3fa694d093e370082d47b0ad63e3598c6511691b
Gabriel_terminal <gabriel.rigonati09@gmail.com>
2026-02-06 21:27:31 -0300

    Finalizando o programa

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ NUMEROS_ESCOLHIDOS = random.sample(range(0, 10), 4)
 tentativas = 0
 
 def titulo():
+    os.system("cls")
     print("""    
 ░█████╗░░█████╗░███████╗██████╗░████████╗███████╗  ░█████╗░  ░██████╗███████╗███╗░░██╗██╗░░██╗░█████╗░██╗
 ██╔══██╗██╔══██╗██╔════╝██╔══██╗╚══██╔══╝██╔════╝  ██╔══██╗  ██╔════╝██╔════╝████╗░██║██║░░██║██╔══██╗██║
@@ -31,13 +32,45 @@ Se Você Colocar [ 1 2 7 8 ] Vai Aparecer ->> Dois Digitos Certos.
 Se Você Colocar [ 1 2 3 8 ] Vai Aparecer ->> Três Digitos Certos. 
 
 Se Você Colocar [ 1 2 3 4 ] Vai Aparecer ->> Parabéns! Você Acertou A Senha.""")
-    input("\nPressione ENTER para começar o jogo")
+    input("\nPressione ENTER para começar o jogo ")
     os.system("cls")
 
+def fim_por_acerto():
+    os.system("cls")
+    print("""
+██████╗░░█████╗░██████╗░░█████╗░██████╗░███████╗███╗░░██╗░██████╗██╗
+██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝████╗░██║██╔════╝██║
+██████╔╝███████║██████╔╝███████║██████╦╝█████╗░░██╔██╗██║╚█████╗░██║
+██╔═══╝░██╔══██║██╔══██╗██╔══██║██╔══██╗██╔══╝░░██║╚████║░╚═══██╗╚═╝
+██║░░░░░██║░░██║██║░░██║██║░░██║██████╦╝███████╗██║░╚███║██████╔╝██╗
+╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚══════╝╚═╝░░╚══╝╚═════╝░╚═╝""")
+    print("\nParabéns! Você Acertou A Senha ->> ", *NUMEROS_ESCOLHIDOS, sep="")
+    print(f"\nE Levou {tentativas} tentativa(s) para acertar!\n")
 
 def rodar_programa():
+    global tentativas
     exibir_tela_inicial()
     print("\nEscolhemos Nossa Senha, Agora Tente Adivinhar!")
+    while True:
+        senha_chutada = input("\nQual Senha Você Acha Que Escolhemos: ")
+        print("\033[A\033[42C", end="") # O cursor sobe uma linha e anda 42 colunas
+        if len(senha_chutada) == 4:
+            acertos = sum(1 for i in range(4) if int(senha_chutada[i]) == NUMEROS_ESCOLHIDOS[i])
+            tentativas += 1
+            if acertos == 0:
+                print("->> Nenhum Digito Certo.")
+            elif acertos == 1:
+                print("->> Um Digito Certo.")
+            elif acertos == 2:
+                print("->> Dois Digitos Certos.")
+            elif acertos == 3:
+                print("->> Três Digitos Certos.")
+            else:
+                fim_por_acerto()
+                break
+        else:
+            print("\n\033[37C", end="")
+            print("└──>> Só 4 Dígitos! Tente Novamente.")
 
 if __name__ == '__main__':
     rodar_programa()
